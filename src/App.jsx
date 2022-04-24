af6ee751a8c02c324c3b9c41f64f141a744b20ac
A-Sawda <sawdaabdoulay@gmail.com>
2022-04-25 00:10:47 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './components/game/Game'
+  
+  // ========================================
+  
+
+  const App = () => {
+    return (
+      <>
+          <Game />
+      </>
+    )
+  }
+  
+  export default App 
+
+  
